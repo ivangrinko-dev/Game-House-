577302cded0eaf40366b753dc48e438d1c9c4a07
ivangrinko-dev <grinko.work.1982@mail.ru>
2023-10-24 21:25:31 +0300

    start project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
-
+import { Route, Routes } from 'react-router-dom'
+import LandingPage from './pages/LandingPage/LandingPage'
 
 function App() {
 
-
   return (
-    <>
-     
-    </>
+    <Routes>
+      <Route path='/' element={<LandingPage />} />
+    </Routes>
   )
 }
 
