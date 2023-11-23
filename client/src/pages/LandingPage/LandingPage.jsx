d7aed22d50acc128448e05cd637eb82157545798
ivangrinko-dev <grinko.work.1982@mail.ru>
2023-11-23 21:14:53 +0300

    finish project

--- client/src/pages/LandingPage/LandingPage.jsx
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -6,10 +6,10 @@ export default function LandingPage() {
     const select = [{ text: 'Payment methods: Skrill,Neteller, webmoney, Banktransfer', date: '10.02.2023' }, { text: 'Payment methods: Skrill,Neteller, webmoney, Banktransfer', date: '10.02.2023' }, { text: 'Payment methods: Skrill,Neteller, webmoney, Banktransfer', date: '10.02.2023' }, { text: 'Payment methods: Skrill,Neteller, webmoney, Banktransfer', date: '10.02.2023' }, { text: 'Payment methods: Skrill,Neteller, webmoney, Banktransfer', date: '10.02.2023' }, { text: 'Payment methods: Skrill,Neteller, webmoney, Banktransfer', date: '10.02.2023' }, { text: 'Payment methods: Skrill,Neteller, webmoney, Banktransfer', date: '10.02.2023' }, { text: 'Payment methods: Skrill,Neteller, webmoney, Banktransfer', date: '10.02.2023' }, { text: 'Payment methods: Skrill,Neteller, webmoney, Banktransfer', date: '10.02.2023' }, { text: 'Payment methods: Skrill, Neteller, webmoney, Bank transfer', date: '10.02.2023' }, { text: 'ayment methods: Skrill, Neteller, webmoney, Bank transfer', date: '10.02.2023' }, { text: 'Payment methods: Skrill, Neteller, webmoney, Bank transfer', date: '10.02.2023' }]
 
     return (
-        <div >
+        <div className={style.backgroundImg}>
             <Header />
 
-             <div className={style.wrapper}>
+            <div className={style.wrapper}>
                 <div className={style.info}>
                     <h1>HOUSE OF</h1>
                     <h1 className={style.titlecolor}>GAMBLING </h1>
@@ -21,11 +21,13 @@ export default function LandingPage() {
 
                 <div className={style.blog}>
                     <h2>BLOG</h2>
-                    <div className={style.payment}>
-                        {select.map((el, index) => <div key={index} className={style.item}><p>{el.text}</p><p>{el.date}</p></div>)}
+                    <div className={style.blogOverflow}>
+                        <div className={style.payment}>
+                            {select.map((el, index) => <div key={index} className={style.item}><p className={style.text}>{el.text}</p><p className={style.date}>{el.date}</p></div>)}
+                        </div>
                     </div>
                 </div>
-            </div> 
+            </div>
 
             <Footer />
         </div>
